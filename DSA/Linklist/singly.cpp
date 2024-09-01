eeba18ce167214a89da781125e350ab1e867f746
Mohit <157598261+LuckyMo-it@users.noreply.github.com>
2024-09-01 12:26:06 +0000

    day 2

---
+++ DSA/Linklist/singly.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdio>
+class node{
+    public:
+    int data;
+    node* next;
+    node()
+    {
+        next=nullptr;
+    }
+};
+class linklist{
+    node *p=new node();
+    
+    void append(int num);
+};
+void linklist::append(int num)
+{
+    node *t,*r;
+}
+int main()
+{
+
+}
---
+++ Language/Pointer/thisNconst.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+// ----------this-------------
+// class behav{public:
+//     int a;
+//         int b;
+
+//     void show(int a=9000)
+//     {
+//         printf("%d\n%d",a,this->a);
+//     }
+// } ;
+
+
+//------------------const---------------
+//const argument
+// void con_test(const char* str)
+// {
+//     printf("you cant change me my buddy %s",str);
+//     // str[0]='p';//gives an error
+// }
+
+//------const variable
+
+
+
+
+int main()
+{
+    // behav that{8,7};
+    // that.show();
+    // con_test("mohit");
+}
